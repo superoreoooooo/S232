8e3c6207873eae15a98af13aca269fdbb6e74d5f
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-09-05 14:21:51 +0900

    hello~~~~~~~~~

--- Java/S232Java/src/main/java/win/oreo/Main.java
+++ Java/S232Java/src/main/java/win/oreo/Main.java
@@ -2,6 +2,8 @@ package win.oreo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world! " + (i + 1));
+        }
     }
 }
