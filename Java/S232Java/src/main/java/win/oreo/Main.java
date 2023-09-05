afeda576bd28c49af4febe2138b10bd787f9cc42
oreo <88177661+superoreoooooo@users.noreply.github.com>
2023-09-05 14:52:40 +0900

    asdf

--- Java/S232Java/src/main/java/win/oreo/Main.java
+++ Java/S232Java/src/main/java/win/oreo/Main.java
@@ -5,5 +5,42 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello world! " + (i + 1));
         }
+
+        A aa = new A(20, "c");
+
+        System.out.println(aa.getA());
+        System.out.println(aa.getB());
+
+        System.out.println(aa);
+    }
+}
+
+class A {
+    private int a;
+    private String b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    public A(int a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+    @Override
+    public String toString() {
+        return a + b;
     }
 }
---
+++ Java/S232Java/src/main/java/win/oreo/self.java
@@ -0,0 +1,9 @@
+package win.oreo;
+
+public class self {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("수고링");
+        System.out.println("ㅋㅋㅋ");
+    }
+}
